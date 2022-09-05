5134739cd1137f17fa3ebbd8dbe781a5c8a214a1
ysaii <112843846+ysaii@users.noreply.github.com>
2022-09-06 00:05:37 +0900

    Update and rename README.md to Test2.java

---
+++ Test2.java
@@ -0,0 +1,32 @@
+# -package com.sh.test;
+
+public class Test2 {
+	public static void main(String[] args) {
+		String str1 = "홍길동";
+		int inum1 = 22;
+		char ch = '남';
+		String str2 = "01012341234";
+		String str3 = "hgd@naver.com";
+		String str4 = "전라남도 광역시";
+		
+		System.out.println("=======================================");
+		System.out.printf("%5s%5d%10s%15s%15s%n", name, age, gender, phoneNum, email, add);
+		System.out.println("=======================================");
+		System.out.printf("%5s%5d%10s%15s%15s%n", name, age, gender, phoneNum, email, add);
+		
+		str1 = "홍길순";
+		inum1 = 20;
+		ch = '여';
+		str2 = "010123412341";
+		str3 = "hgs@gmail.com ";
+		str4 = "전라남도 광주시";
+		
+		System.out.println("%5s%5d%10s%15s%15s%n", name, age, gender, phoneNum, email, add);
+	
+		
+		
+		
+		
+	}
+
+}
